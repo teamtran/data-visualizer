d4bdf10e7b31c108a5f6687b55aa847844362e01
stanlo229 <stanley.lo@mail.utoronto.ca>
2025-02-24 21:01:27 -0500

    working on dynamic TGA results

---
+++ scripts/GPC/figure_Xmw_study_run.py
@@ -0,0 +1,91 @@
+from pathlib import Path
+import sys
+from matplotlib import pyplot as plt
+import json
+
+# sets the path to the root of the repository
+root_path = Path(__file__).parent.parent.parent.resolve()
+sys.path.insert(0, str(root_path))
+
+from code.GPC.gpc_plots import GPCPlots
+
+# Style path
+style_path: Path = root_path / "style" / "style.json"
+# read style file
+style: dict = json.loads(style_path.read_text())
+# defines font, fontsize, and axes linewidth of all of the following plots
+plt.rcParams["font.family"] = style["fontfamily"]
+plt.rcParams["font.size"] = style["fontsize"]
+plt.rcParams["axes.linewidth"] = style["axes_linewidth"]
+
+# TODO: Change experiment name and filenames
+# GPC Curves
+# Experiment Name
+experiment_name: str = "005d-L_M"
+
+# Result path
+result_path: Path = root_path / "results" / "GPC" / experiment_name
+
+# Data directory path
+data_dir: Path = root_path / "data" / "GPC" / experiment_name
+
+# Filenames
+gpc_filenames: list[str] = [
+    # "2024_06_23_PS2439_6_1K.txt",
+    # TODO: need to re-run all of these on same day.
+    "2025_02_11_PS_Tosoh_A1000.txt",
+    "2025_02_11_PS_Tosoh_F1.txt",
+    "2025_02_11_PS_Tosoh_F2.txt",
+    "2025_02_11_PS_Tosoh_F4.txt",
+    "2025_02_11_PS_Tosoh_F10.txt",
+    "2025_02_11_005d-M1.txt",
+    "2025_02_11_005d-L1.txt",
+    "2025_02_11_005d-L2.txt",
+    "2025_02_11_005d-L3.txt",
+    "2025_02_11_005d-L4.txt",
+]
+
+labels: list = [
+    "PS-1.12K",
+    "PS-10K",
+    "PS-19.6K",
+    "PS-40.4K",
+    "PS-110K",
+    "PS-1.1K-SCF3",
+    "PS-10K-SCF3",
+    "PS-19.6K-SCF3",
+    "PS-40.4K-SCF3",
+    "PS-110K-SCF3",
+]
+
+gpc_metadata: list = [
+    # "Mn=54.5k, Mw=121.3k, Mp=100k",  # PS-Styrofoam
+    # "Mn=48.1k, Mw=140k, Mp=92.2k" # PS-Styrofoam-SCF3
+    "Mn=0.7k, Mw=0.9k, Mp=1.0k",  # PS-1.12K
+    "Mn=8.5k, Mw=9.2k, Mp=9.8k",  # PS-10K
+    "Mn=16.7k, Mw=17.9k, Mp=19.5k",  # PS-19.6K
+    "Mn=34.6k, Mw=37.4k, Mp=41.4k",  # PS-40.4K
+    "Mn=94.0k, Mw=103.0k, Mp=111.2k",  # PS-110K
+    "Mn=0.9k, Mw=1.1k, Mp=1.0k",  # PS-1.1K-SCF3
+    "Mn=9.5k, Mw=10.2k, Mp=10.8k",  # PS-10K-SCF3
+    "Mn=17.2k, Mw=18.4k, Mp=19.6k",  # PS-19.6K-SCF3
+    "Mn=33.8k, Mw=36.8k, Mp=40.7k",  # PS-40.4K-SCF3
+    "Mn=84.9k, Mw=100k, Mp=109k",  # PS-110K-SCF3
+]
+
+if __name__ == "__main__":
+    gpc_plots = GPCPlots(
+        data_dir=data_dir,
+        gpc_data_path=gpc_filenames,
+        labels=labels,
+        colors=["#8286ff", "#f5c92a", "#e0f3db", "#7bccc4"],
+        result_dir=result_path,
+        style_path=style_path,
+    )
+    gpc_plots.plot_gpc(
+        gpc_metadata=gpc_metadata,
+        xlim=(6.75, 8.5),
+        ylim=(-0.1, 1.1),
+        inset_xlim=(7.65, 7.82),
+        rt=7.25,
+    )
